09da2d2fe6f151d7e916fab86ff060ffe946a291
GeorgeDoLee <Gio.dolidze1234@gmail.com>
2023-08-21 22:31:25 +0400

    loading screens updated

--- Caucasus-University-Forum/react/src/components/SearchingProfiles.js
+++ Caucasus-University-Forum/react/src/components/SearchingProfiles.js
@@ -4,17 +4,17 @@ import LoadingSearchedUsers from './loading-components/LoadingSearchedUsers';
 import useFetch from '../hooks/useFetch';
 
 const SearchingProfiles = ({searchQuery}) => {
-    const { data: users, isPending, error } = useFetch('/posts', searchQuery); 
+    // const { data: users, isPending, error } = useFetch(); 
     //აქ ზემოთ იუზერები ამოიღე როგორც უნდა, სხვა იუზერები ზოგადად არა ის რომელიც ავტორიზებულია და ამას ეძებს
 
     return (  
             <div className="searched-profiles">
             {/* // {users.map((user) => ( */}
-            
-                <LoadingSearchedUsers />
+{/*             
+                {isPending && <LoadingSearchedUsers />}
+                {error && {error}} */}
 
-
-                {/* <div className='user'>
+                <div className='user'>
                     <img src={ProfilePhoto} alt="photo not found" />
                     <div className="user-details">
                         <h3 className='user-name'>username</h3>
@@ -64,7 +64,7 @@ const SearchingProfiles = ({searchQuery}) => {
                         <h3 className='user-name'>username</h3>
                         <h5 className='user-school'>cst</h5>
                     </div>
-                </div> */}
+                </div>
             {/* // )} */}
             </div>
     );
--- Caucasus-University-Forum/react/src/components/loading-components/LoadingSearchedUsers.js
+++ Caucasus-University-Forum/react/src/components/loading-components/LoadingSearchedUsers.js
@@ -1,12 +1,12 @@
-
+import '../../css/loading-searched-users.css';
 const LoadingSearchedUsers = () => {
     return (  
-        Array.from({length: 9}).map(() => 
+        Array.from({length: 6}).map(() => 
             <div className='loading-searched-user'>
                 <div className="loading-user-photo"></div>
                 <div className="loading-user-details">
-                    <h3 className='loadijg-user-name'>username</h3>
-                    <h5 className='loading-user-school'>cst</h5>
+                    <div className='loading-user-name'></div>
+                    <div className='loading-user-school'></div>
                 </div>
             </div>
         )
